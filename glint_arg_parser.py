fa6776b100d4456163afb94c3c10fb819dc322c5
rd <rd@rat01.heprc.uvic.ca>
2014-09-26 15:31:43 -0700

    added stunnel script

--- glint_arg_parser.py
+++ glint_arg_parser.py
@@ -20,4 +20,8 @@ class GlintArgumentParser:
         self.parser.add_argument("-glint_horizon_server",nargs=1,choices=['django','apache'])
         self.parser.add_argument("-glint_user_id")
 
+    def init_stunnel_arg_parser(self):
+        self.parser.add_argument("--install",action="store_true")
+        self.parser.add_argument("--uninstall",action="store_true")
+
 
